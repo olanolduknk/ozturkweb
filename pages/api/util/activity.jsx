4f69e91e9f9ea7513af0d05139dcee935a936255
Öztürk <13538249+ozturkbey@users.noreply.github.com>
2023-03-25 08:14:58 -0700

    update

--- pages/api/util/activity.jsx
+++ pages/api/util/activity.jsx
@@ -2,6 +2,6 @@ import fetch from 'isomorphic-unfetch';
 
 export default async function Activity(req, res) {
     res.json(
-        await (await fetch('https://linkcord.js.org/api/v3/user/314403590942752778')).json()
+        await (await fetch('https://linkcord.js.org/api/v3/user/245511350724329473')).json()
     );
 };
--- pages/api/util/me.jsx
+++ pages/api/util/me.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default async function Repos(req, res) {
-    let linkcord = await axios.get('https://linkcord.js.org/api/v3/user/314403590942752778').catch(() => {});
+    let linkcord = await axios.get('https://linkcord.js.org/api/v3/user/245511350724329473').catch(() => {});
     res.json((linkcord && linkcord.data) ? linkcord.data.data || {} : {});
 };
