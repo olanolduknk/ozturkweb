783d1595f4b18d37facc2fa52eb8a3d804ce326c
Öztürk <13538249+ozturkbey@users.noreply.github.com>
2023-03-25 08:08:46 -0700

    update

--- components/About/Text.jsx
+++ components/About/Text.jsx
@@ -1,5 +1,5 @@
 export default function Text() {
     return (
-        <p className="leading-5 text-justify tracking-wide text-lg text-gray-100 italic">Hello, my name is Ozturk. I am 21 years old and a high school graduate. I'm a Full Stack developer. I have many projects and applications. ^^</p>
+        <p className="leading-5 text-justify tracking-wide text-lg text-gray-100 italic">My name is Öztürk. I live in Turkey. I'm 21 years old and I'm a Full Stack developer. Building websites, bots, npm packages and more. If you want to see my resume or want to contact me, please send me an e-mail. ilovefourleafclover@icloud.com ^^</p>
     );
 };
