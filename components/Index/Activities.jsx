05186e5ed30db1ae9baaba11f497a90fca8d0666
Öztürk <13538249+ozturkbey@users.noreply.github.com>
2023-04-05 07:44:11 +0600

    1

--- components/Index/Activities.jsx
+++ components/Index/Activities.jsx
@@ -49,24 +49,6 @@ export default function Activities() {
                     <iframe src={"https://linkcord.js.org/api/v3/widget/245511350724329473?type=spotify_large&lang=en&theme=dark&align=left&background=080808"} className="w-full" height="140" allowtransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" />
                 </div>}
             </div>
-
-
-
-            <div className="flex items-center space-x-5 rounded-lg bg-[#080808] p-5">
-                <div className="relative flex items-center w-full h-[140px]">
-                    <div className="absolute bottom-1 bg-[#080808] rounded-full right-1 w-6 h-6 flex items-center justify-center">
-                            <iframe src={"https://linkcord.js.org/api/v3/widget/245511350724329473?type=status"}  />
-                            </div>
-                </div>
-            </div>
-            <div className="mt-5 md:mt-0 rounded-lg overflow-hidden col-span-2 w-full h-[140px] sm:gap-x-6 py-5 relative">
-                <div className="space-x-3 absolute inset-0 bg-[#080808] rounded-lg flex items-center justify-center">
-                    <iframe src={"https://linkcord.js.org/api/v3/widget/245511350724329473?type=vsc&lang=en&theme=dark&align=left&background=080808"} className="w-full" height="140" allowtransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" />
-                </div>
-            </div>
-
-
-
         </div>
     );
 };
