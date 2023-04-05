0e4e8f7ed462a7136389bf12121557fc9140cafc
Öztürk <13538249+ozturkbey@users.noreply.github.com>
2023-04-05 06:59:31 +0600

    1

--- components/Index/Activities.jsx
+++ components/Index/Activities.jsx
@@ -55,7 +55,7 @@ export default function Activities() {
             <div className="flex items-center space-x-5 rounded-lg bg-[#080808] p-5">
                 <div className="relative flex items-center w-full h-[140px]">
                     <div className="absolute bottom-1 bg-[#080808] rounded-full right-1 w-full h-[140px] flex items-center justify-center">
-                            <iframe src={"https://linkcord.js.org/api/v3/widget/245511350724329473?type=status"} className="w-full" height="140" allowtransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" />
+                            <iframe src={"https://linkcord.js.org/api/v3/widget/245511350724329473?type=status&lang=en&theme=dark&align=left&background=080808"} />
                             </div>
                 </div>
             </div>
