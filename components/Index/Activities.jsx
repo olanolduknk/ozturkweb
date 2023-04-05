57148c2c847d9927e08ba536a130b3df4a51e800
Öztürk <13538249+ozturkbey@users.noreply.github.com>
2023-04-05 21:13:22 +0600

    1

--- components/Index/Activities.jsx
+++ components/Index/Activities.jsx
@@ -50,22 +50,17 @@ export default function Activities() {
                 </div>}
             </div>
 
+
+
+
+
             <div className="flex items-center space-x-5 rounded-lg bg-[#080808] p-5">
                 <div className="relative flex items-center w-[100px] h-[100px]">
-                    {!profile ? (
-                        <img className="animate-pulse rounded-full w-auto" src="https://i.ibb.co/68X2Xfq/pulse.png" />
-                    ) : (
-                        <img className="rounded-full w-auto" src={`https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}?size=4096`} />
-                    )}
-                    {profile && <Tippy interactive={true} content={profile.status.toUpperCase()}>
-                        <div className="absolute bottom-1 bg-[#080808] rounded-full right-1 w-6 h-6 flex items-center justify-center">
-                            <div className="w-full h-full relative flex items-center justify-center">
-                                <div className={"animate-ping w-3 h-3 rounded-full " + `discord-${profile.status}`} />
-                                <div className={"absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-3 h-3 rounded-full " + `discord-${profile.status}`} />
-                            </div>
-                        </div>
-                    </Tippy>}
+                <iframe src="https://linkcord.js.org/api/v3/widget/245511350724329473?type=vsc" width="600" height="150" allowtransparency="true" frameborder="0" />
                 </div>
+
+
+
                 {!profile ? <div className="flex-1">
                     <div className="animate-pulse bg-white/10 w-16 h-6 rounded-lg" />
                     <div className="animate-pulse mt-2 bg-white/10 w-12 h-4 rounded-md" />
