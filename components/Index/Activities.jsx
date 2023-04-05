2fcdbc2712b498b3e0844dd04b059be5b4d4b825
Öztürk <13538249+ozturkbey@users.noreply.github.com>
2023-04-06 00:25:30 +0600

    1

--- components/Index/Activities.jsx
+++ components/Index/Activities.jsx
@@ -56,20 +56,17 @@ export default function Activities() {
 
             <div className="flex items-center space-x-5 rounded-lg bg-[#080808] p-5">
             <div className="relative flex items-center w-[100px] h-[100px]">
-                    {!profile ? (
-                        <img className="animate-pulse rounded-full w-auto" src="https://i.ibb.co/68X2Xfq/pulse.png" />
-                    ) : (
-                        <img className="rounded-full w-auto" src={`https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}?size=4096`} />
-                    )}
-                </div>
-                {!profile ? <div className="flex-1">
+            <iframe src="https://linkcord.js.org/api/v3/widget/245511350724329473?type=status" width="300" height="100" allowtransparency="true" frameborder="0" />
+                  <div className="flex-1">
                     <div className="animate-pulse bg-white/10 w-16 h-6 rounded-lg" />
                     <div className="animate-pulse mt-2 bg-white/10 w-12 h-4 rounded-md" />
                 </div> : <div className="flex-1">
                     <h1 className="text-center leading-none font-light text-3xl md:text-xl lg:text-3xl text-white">{profile.username}</h1>
                     <h1 className="text-center leading-none font-semibold text-xl md:text-lg lg:text-xl text-zinc-300">#{profile.discriminator}</h1>
-                </div>}
+                </div>
             </div>
+
+
 
             <div className="mt-5 md:mt-0 rounded-lg overflow-hidden col-span-2 w-full h-[140px] sm:gap-x-6 py-5 relative">
                 <div className="space-x-3 absolute inset-0 bg-[#080808] rounded-lg flex items-center justify-center">
