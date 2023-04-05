ca48f3c1ac0c00775757f886a7c1abb4f08ed72d
Öztürk <13538249+ozturkbey@users.noreply.github.com>
2023-04-05 23:39:43 +0600

    1

--- components/Index/Activities.jsx
+++ components/Index/Activities.jsx
@@ -55,11 +55,29 @@ export default function Activities() {
 
 
             <div className="flex items-center space-x-5 rounded-lg bg-[#080808] p-5">
-                <div className="relative flex items-center w-[100px] h-[100px]">
-                <iframe src="https://linkcord.js.org/api/v3/widget/245511350724329473?type=status&theme=dark&background=080808" width="300" height="100" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"/>
+            <div className="relative flex items-center w-[100px] h-[100px]">
+                    {!profile ? (
+                        <img className="animate-pulse rounded-full w-auto" src="https://i.ibb.co/68X2Xfq/pulse.png" />
+                    ) : (
+                        <img className="rounded-full w-auto" src={`https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}?size=4096`} />
+                    )}
+                    {profile && <Tippy interactive={true} content={profile.status.toUpperCase()}>
+                        <div className="absolute bottom-1 bg-[#080808] rounded-full right-1 w-6 h-6 flex items-center justify-center">
+                            <div className="w-full h-full relative flex items-center justify-center">
+                                <div className={"animate-ping w-3 h-3 rounded-full " + `discord-${profile.status}`} />
+                                <div className={"absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-3 h-3 rounded-full " + `discord-${profile.status}`} />
+                            </div>
+                        </div>
+                    </Tippy>}
                 </div>
+                {!profile ? <div className="flex-1">
+                    <div className="animate-pulse bg-white/10 w-16 h-6 rounded-lg" />
+                    <div className="animate-pulse mt-2 bg-white/10 w-12 h-4 rounded-md" />
+                </div> : <div className="flex-1">
+                    <h1 className="text-center leading-none font-light text-3xl md:text-xl lg:text-3xl text-white">{profile.username}</h1>
+                    <h1 className="text-center leading-none font-semibold text-xl md:text-lg lg:text-xl text-zinc-300">#{profile.discriminator}</h1>
+                </div>}
             </div>
-
 
             <div className="mt-5 md:mt-0 rounded-lg overflow-hidden col-span-2 w-full h-[140px] sm:gap-x-6 py-5 relative">
                 <div className="space-x-3 absolute inset-0 bg-[#080808] rounded-lg flex items-center justify-center">
