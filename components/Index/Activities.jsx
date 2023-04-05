8614bc35e3828eae8fbb570f0757408e1fcb4480
Öztürk <13538249+ozturkbey@users.noreply.github.com>
2023-04-06 02:20:46 +0600

    1

--- components/Index/Activities.jsx
+++ components/Index/Activities.jsx
@@ -74,10 +74,13 @@ export default function Activities() {
                     <div className="animate-pulse bg-white/10 w-16 h-6 rounded-lg" />
                     <div className="animate-pulse mt-2 bg-white/10 w-12 h-4 rounded-md" />
                 </div> : <div className="flex-1">
-                    <h1 className="text-center leading-none font-light text-3xl md:text-xl lg:text-3xl text-white">{profile.username}</h1>
-                    <h1 className="text-center leading-none font-semibold text-xl md:text-lg lg:text-xl text-zinc-300">#{profile.discriminator}</h1>
+                    <h1 className="text-center leading-none font-light text-3xl md:text-xl lg:text-3xl text-white"></h1>
+                    <h1 className="text-center leading-none font-semibold text-xl md:text-lg lg:text-xl text-zinc-300"></h1>
                 </div>}
             </div>
+
+
+
 
 
             <div className="mt-5 md:mt-0 rounded-lg overflow-hidden col-span-2 w-full h-[140px] sm:gap-x-6 py-5 relative">
