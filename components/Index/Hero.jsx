1da264d088e97ba1ce3d897a6b9b72bc53b6313f
Öztürk <13538249+ozturkbey@users.noreply.github.com>
2023-04-09 21:01:30 +0600

    update

--- components/Index/Hero.jsx
+++ components/Index/Hero.jsx
@@ -95,7 +95,30 @@ export default function Hero() {
                     </>}
                 </div>
                 <div className={(!projects || !randomThree ? "animate-pulse" : "") + " w-32 h-16 absolute top-[35%] left-[50%] sm:left-[40%] project-box tone-3 p-3 rounded-lg perspective-middle flex items-center space-x-2"}>
-                <iframe src={"https://linkcord.js.org/api/v3/widget/245511350724329473?type=spotify_large&lang=en&theme=dark&align=left&background=080808"} className="w-full" height="140" allowtransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" />
+                <div className="flex items-center space-x-5 rounded-lg bg-[#080808] p-5">
+                <div className="relative flex items-center w-[100px] h-[100px]">
+                    {!profile ? (
+                        <img className="animate-pulse rounded-full w-auto" src="https://i.ibb.co/68X2Xfq/pulse.png" />
+                    ) : (
+                        <img className="rounded-full w-auto" src={`https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}?size=4096`} />
+                    )}
+                    {profile && <Tippy interactive={true} content={profile.status.toUpperCase()}>
+                        <div className="absolute bottom-1 bg-[#080808] rounded-full right-1 w-6 h-6 flex items-center justify-center">
+                            <div className="w-full h-full relative flex items-center justify-center">
+                                <div className={"animate-ping w-3 h-3 rounded-full " + `discord-${profile.status}`} />
+                                <div className={"absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-3 h-3 rounded-full " + `discord-${profile.status}`} />
+                            </div>
+                        </div>
+                    </Tippy>}
+                </div>
+                {!profile ? <div className="flex-1">
+                    <div className="animate-pulse bg-white/10 w-16 h-6 rounded-lg" />
+                    <div className="animate-pulse mt-2 bg-white/10 w-12 h-4 rounded-md" />
+                </div> : <div className="flex-1">
+                    <h1 className="text-center leading-none font-light text-3xl md:text-xl lg:text-3xl text-white">{profile.username}</h1>
+                    <h1 className="text-center leading-none font-semibold text-xl md:text-lg lg:text-xl text-zinc-300">#{profile.discriminator}</h1>
+                </div>}
+            </div>
                 </div>
             </div>
         </div>
