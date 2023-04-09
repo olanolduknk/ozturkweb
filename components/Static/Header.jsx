7f6554ef8751b22ad3a5d717b1154af29644d3cc
Öztürk <13538249+ozturkbey@users.noreply.github.com>
2023-04-09 07:15:42 +0600

    1

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -142,13 +142,13 @@ export default function Header() {
                 >
                     <img className="w-6 h-6 translate-y-3 -translate-x-2 -rotate-12" src={baseURL + emojis[Object.keys(emojis)[Math.floor(Math.random() * Object.keys(emojis).length)]]} />
                 </Transition>
-                <Tippy interactive={true} content="mal">
+                <Tippy interactive={true} content="duygusal">
                     <button onClick={() => {
                         if (isClicked) return;
                         setIsClicked(true);
                         setTimeout(() => setIsClicked(false), 1000);
                     }} className="focus:outline-none cursor-pointer hidden lg:block version text-sm py-1.5 px-3 rounded-md">
-                        <span className="font-bold">Devin#6515</span>
+                        <span className="font-bold">aledora#2000</span>
                     </button>
                 </Tippy>
             </div>
