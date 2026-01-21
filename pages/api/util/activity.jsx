8a62b2baffb829ec2f3b0ef3cf43be24e8a78a4e
olanolduknk <13538249+olanolduknk@users.noreply.github.com>
2026-01-22 01:34:30 +0700

    new

--- pages/api/util/activity.jsx
+++ pages/api/util/activity.jsx
@@ -2,6 +2,6 @@ import fetch from 'isomorphic-unfetch';
 
 export default async function Activity(req, res) {
     res.json(
-        await (await fetch('https://linkcord.js.org/api/v3/user/245511350724329473')).json()
+        await (await fetch('https://linkcord.js.org/api/v3/user/1442201028048060529')).json()
     );
 };
